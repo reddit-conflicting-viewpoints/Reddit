4406a0eb2435151bad0604b7002133ea7631b231
azhou5211 <ajz55@uw.edu>
2022-01-20 13:40:56 -0800

    Ran LDA on bigger datasets of computerscience and Music. Modified config to include Music.

--- src/data/config.py
+++ src/data/config.py
@@ -8,6 +8,6 @@ class ScraperConfig:
 	max_post_count: int #number of hot posts to retrieve
 	max_comment_count: int #number of top comments associated with each retrieved post
 
-default_config = scraper_config = ScraperConfig(subreddit_list=['computerscience'],
+default_config = scraper_config = ScraperConfig(subreddit_list=['computerscience', 'Music'],
 												max_post_count = 10000,
 												max_comment_count = 500)
