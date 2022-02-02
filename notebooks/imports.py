9e39f04c537542848f819d3675282c2b8e62c866
smuktevi <vmuktev1@uw.edu>
2022-02-01 16:45:36 -0800

    basic sentiment analysis for posts and comments done using bert

--- notebooks/imports.py
+++ notebooks/imports.py
@@ -6,19 +6,28 @@ import itertools
 from wordcloud import WordCloud, STOPWORDS
 
 from src.features.preprocess import PreProcess
-from src.features.preprocess_old import PreProcess
+# from src.features.preprocess_old import PreProcess
 
 from pprint import pprint
 from bertopic import BERTopic
 
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.probability import FreqDist
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 import re
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+import requests
+from bs4 import BeautifulSoup
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
 
 from gensim import corpora, models
 import gensim
