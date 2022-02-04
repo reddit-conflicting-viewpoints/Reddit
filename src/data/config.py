c04e450209f868c9fa00976155d221554c070e9c
azhou5211 <ajz55@uw.edu>
2022-02-04 13:22:08 -0800

    modified scraper to pull data in different orders. Also added ability to scrape comments of comments

--- src/data/config.py
+++ src/data/config.py
@@ -1,13 +1,19 @@
 # config file for scraper
 from dataclasses import dataclass
 from typing import List
+import numpy as np
+
 
 @dataclass(frozen=True)
 class ScraperConfig:
-	subreddit_list: List[str] #list of subreddits to scrape
-	max_post_count: int #number of hot posts to retrieve
-	max_comment_count: int #number of top comments associated with each retrieved post
+    subreddit_list: List[str]  # list of subreddits to scrape
+    scrape_order: str  # order to scrape the data from. Can take values: 'hot', 'new', 'top', 'rising'. Notes: 'top' scrapes top of all time.
+    max_post_count: int  # number of hot posts to retrieve
+    max_comment_count: int  # number of top comments associated with each retrieved post
 
-default_config = scraper_config = ScraperConfig(subreddit_list=['computerscience', 'Music'],
-												max_post_count = 10000,
-												max_comment_count = 500)
+
+default_config = scraper_config = ScraperConfig(
+    subreddit_list=['computerscience'],
+    scrape_order='new',
+    max_post_count=10000,
+    max_comment_count=np.inf)
--- src/data/scraper.py
+++ src/data/scraper.py
@@ -10,22 +10,23 @@ from src.utils import get_project_root
 from contextlib import asynccontextmanager
 from config import ScraperConfig, default_config
 
-#reddit API credentials
+# reddit API credentials
 CLIENT_ID = 'xG2uYfBViT_APANuInp5Yw'
 CLIENT_SECRET = 'ewGDf8V8LFdgCuH0VxmYUehgKq18ug'
 USER_AGENT = 'uw_msds_2021fall'
 
-#logger setup
+# logger setup
 handler = logging.StreamHandler(sys.stdout)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
+
 @dataclass(frozen=True)
 class SubRedditData:
-    name:str
-    posts_df:pd.DataFrame = field(repr=False)
-    comments_df:pd.DataFrame = field(repr=False)
+    name: str
+    posts_df: pd.DataFrame = field(repr=False)
+    comments_df: pd.DataFrame = field(repr=False)
 
 
 class AsyncRedditScraper:
@@ -35,9 +36,9 @@ class AsyncRedditScraper:
 
     @asynccontextmanager
     async def _generate_session(self):
-        session =  asyncpraw.Reddit(client_id = CLIENT_ID,
-                             client_secret = CLIENT_SECRET,
-                             user_agent = CLIENT_SECRET)
+        session = asyncpraw.Reddit(client_id=CLIENT_ID,
+                                   client_secret=CLIENT_SECRET,
+                                   user_agent=CLIENT_SECRET)
         try:
             yield session
         except Exception:
@@ -52,34 +53,65 @@ class AsyncRedditScraper:
             posts_retrieved = []
             comments_retrieved = []
             subreddit = await session.subreddit(subreddit_name)
-            async for post in subreddit.hot(limit = self.config.max_post_count):
+
+            scrape_order = {
+                'hot': subreddit.hot,
+                'new': subreddit.new,
+                'top': subreddit.top,
+                'rising': subreddit.rising
+            }
+
+            async for post in scrape_order[self.config.scrape_order](limit=self.config.max_post_count):
                 posts_retrieved.append([post.id,
-                         post.title,
-                         post.score,
-                         post.upvote_ratio,
-                         post.subreddit,
-                         post.url,
-                         post.num_comments,
-                         post.selftext,
-                         post.created])
+                                        post.title,
+                                        post.score,
+                                        post.upvote_ratio,
+                                        post.subreddit,
+                                        post.url,
+                                        post.num_comments,
+                                        post.selftext,
+                                        post.created])
                 comments = await post.comments()
-                await comments.replace_more(0)
-                for comment_idx in range(min(len(comments),self.config.max_comment_count)):
-                        comments_retrieved.append([post.id, comments[comment_idx].body])
+                await comments.replace_more(limit=None)
+                all_comments = await comments.list()
+
+                # DFS of the comments. (Appears in the same order as reddit)
+                comment_queue = comments[:]
+                comment_idx = 0
+                while comment_queue and comment_idx < self.config.max_comment_count:
+                    comment = comment_queue.pop(0)
+                    comments_retrieved.append([
+                        post.id,
+                        comment.body,
+                        comment.id,
+                        comment.parent_id,
+                        comment.created_utc,
+                        comment.is_submitter])
+                    comment_queue[0:0] = comment.replies
+                    comment_idx += 1
+
+                # BFS
+                # counter = 0
+                # for comment in all_comments:
+                #     if counter == self.config.max_comment_count:
+                #         break
+                #     comments_retrieved.append([post.id, comment.body, comment.parent_id])
+                #     counter += 1
+
         posts_df = pd.DataFrame(posts_retrieved,
                                columns=['post_id', 'title', 'score', 'upvote_ratio', 'subreddit', 'url',
                                         'num_comments', 'body', 'created'])
         comments_df = pd.DataFrame(comments_retrieved,
-                                  columns=['post_id','comment'])
+                                  columns=['post_id', 'comment', 'comment_id', 'parent_id', 'created', 'is_submitter'])
         logger.info(f'done scraping subreddit: {subreddit_name}')
         return SubRedditData(subreddit_name, posts_df, comments_df)
 
     async def scrape(self):
-        logger.info(f'start scraping top {self.config.max_post_count} hotest posts with '
+        logger.info(f'start scraping top {self.config.max_post_count} hottest posts with '
                    f'{self.config.max_comment_count} comments per post from the following subreddits: '
                     f'{", ".join(self.config.subreddit_list)}')
         self._data = await asyncio.gather(*(self._fetch_from_single_subreddit(subreddit_name) for
-                                         subreddit_name in self.config.subreddit_list))
+                                            subreddit_name in self.config.subreddit_list))
         return self
 
     def save_to_file(self):
@@ -90,7 +122,7 @@ class AsyncRedditScraper:
         p.mkdir(parents=True, exist_ok=True)
         for data_obj in self._data:
             data_obj.posts_df.to_csv(p.joinpath(f'{data_obj.name}_posts.csv'), index=False)
-            data_obj.comments_df.to_csv(p.joinpath(f'{data_obj.name}_comments.csv'), index = False)
+            data_obj.comments_df.to_csv(p.joinpath(f'{data_obj.name}_comments.csv'), index=False)
         return self
 
 
