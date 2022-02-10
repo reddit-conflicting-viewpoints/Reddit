6568db580088fea5e1110f10b9fe313032db1977
Andy <azhou5211@gmail.com>
2022-02-10 17:21:18 -0500

    manually merged scraper codes together

--- src/data/config.py
+++ src/data/config.py
@@ -8,12 +8,14 @@ import numpy as np
 class ScraperConfig:
     subreddit_list: List[str]  # list of subreddits to scrape
     scrape_order: str  # order to scrape the data from. Can take values: 'hot', 'new', 'top', 'rising'. Notes: 'top' scrapes top of all time.
+    tree: bool # scrape comments of comments if true. scrape top level comments only if false
     max_post_count: int  # number of hot posts to retrieve
     max_comment_count: int  # number of top comments associated with each retrieved post
 
 
 default_config = scraper_config = ScraperConfig(
-    subreddit_list=['computerscience'],
+    subreddit_list=['lotr'],
     scrape_order='new',
-    max_post_count=10000,
-    max_comment_count=np.inf)
+    tree=True,
+    max_post_count=10,
+    max_comment_count=10)
--- src/data/scraper.py
+++ src/data/scraper.py
@@ -6,23 +6,25 @@ import asyncpraw
 import pandas as pd
 from dataclasses import dataclass, field
 from typing import List
-from pathlib import Path
+from src.utils import get_project_root
 from contextlib import asynccontextmanager
 from config import ScraperConfig, default_config
 
-#reddit API credentials
+# reddit API credentials
 CLIENT_ID = 'xG2uYfBViT_APANuInp5Yw'
 CLIENT_SECRET = 'ewGDf8V8LFdgCuH0VxmYUehgKq18ug'
 USER_AGENT = 'uw_msds_2021fall'
 
-#logger setup
+# logger setup
 handler = logging.StreamHandler(sys.stdout)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(handler)
 
+
 class TerminalError(Exception):
     pass
+
 
 def async_retry(times=3, delay=0):
     """
@@ -40,12 +42,12 @@ def async_retry(times=3, delay=0):
     return func_wrapper
 
 
-
 @dataclass(frozen=True)
 class SubRedditData:
-    name:str
-    posts_df:pd.DataFrame = field(repr=False)
-    comments_df:pd.DataFrame = field(repr=False)
+    name: str
+    posts_df: pd.DataFrame = field(repr=False)
+    comments_df: pd.DataFrame = field(repr=False)
+
 
 class AsyncRedditScraper:
     def __init__(self, config: ScraperConfig):
@@ -54,9 +56,9 @@ class AsyncRedditScraper:
 
     @asynccontextmanager
     async def _generate_session(self):
-        session =  asyncpraw.Reddit(client_id = CLIENT_ID,
-                             client_secret = CLIENT_SECRET,
-                             user_agent = CLIENT_SECRET)
+        session =  asyncpraw.Reddit(client_id=CLIENT_ID,
+                                    client_secret=CLIENT_SECRET,
+                                    user_agent=CLIENT_SECRET)
         try:
             yield session
         except Exception:
@@ -70,58 +72,108 @@ class AsyncRedditScraper:
             posts_retrieved = []
             comments_retrieved = []
             subreddit = await session.subreddit(subreddit_name)
-            posts =[post async for post in
-                    subreddit.hot(limit = self.config.max_post_count)
-                    if not post.stickied]
+
+            scrape_order = {
+                'hot': subreddit.hot,
+                'new': subreddit.new,
+                'top': subreddit.top,
+                'rising': subreddit.rising
+            }
+
+            posts = [post async for post in
+                     scrape_order[self.config.scrape_order](limit=self.config.max_post_count)
+                     if not post.stickied]
 
             for post in posts:
                 posts_retrieved.append([post.id,
-                        post.title,
-                        post.score,
-                        post.subreddit,
-                        post.url,
-                        post.num_comments,
-                        post.selftext,
-                        post.created])
+                                        post.title,
+                                        post.score,
+                                        post.upvote_ratio,
+                                        post.subreddit,
+                                        post.url,
+                                        post.num_comments,
+                                        post.selftext,
+                                        post.created])
 
-            comment_forests = await asyncio.gather(*[post.comments() for post in posts])
-            await asyncio.gather(*[comment_forest.replace_more(0) for comment_forest in comment_forests])
-            #in place
-            for comment_forest in comment_forests:
-                top_comments = comment_forest.list()[:self.config.max_comment_count]
-                while top_comments:
-                    comment = top_comments.pop()
-                    if comment.stickied:
-                        continue
-                    comments_retrieved.append([comment.submission.id,
-                                                comment.name,
-                                            comment.body,
-                                            comment.ups,
-                                            comment.downs,
-                                            comment.controversiality,
-                                            comment.total_awards_received,
-                                            comment.score,
-                                            comment.locked,
-                                            comment.collapsed]
-                                            )
+            if self.config.tree:
+                comment_forests = await asyncio.gather(*[post.comments() for post in posts])
+                await asyncio.gather(*[comment_forest.replace_more(limit=None) for comment_forest in comment_forests])
+
+                for comment_forest in comment_forests:
+
+                    # DFS of the comments
+                    comment_queue = comment_forest[:]
+                    comment_idx = 0
+                    while comment_queue and comment_idx < self.config.max_comment_count:
+                        comment = comment_queue.pop(0)
+                        if comment.stickied:
+                            continue
+                        comments_retrieved.append([post.id,
+                                                   comment.id,
+                                                   comment.parent_id,
+                                                   comment.name,
+                                                   comment.body,
+                                                   comment.ups,
+                                                   comment.downs,
+                                                   comment.controversiality,
+                                                   comment.total_awards_received,
+                                                   comment.score,
+                                                   comment.locked,
+                                                   comment.collapsed,
+                                                   comment.is_submitter,
+                                                   comment.created_utc])
+                        comment_queue[0:0] = comment.replies
+                        comment_idx += 1
+            else:
+                comment_forests = await asyncio.gather(*[post.comments() for post in posts])
+                await asyncio.gather(*[comment_forest.replace_more(0) for comment_forest in comment_forests])
+                # in place
+                for comment_forest in comment_forests:
+                    top_comments = comment_forest.list()[:self.config.max_comment_count]
+                    while top_comments:
+                        comment = top_comments.pop()
+                        if comment.stickied:
+                            continue
+                        comments_retrieved.append([post.id,
+                                                   comment.id,
+                                                   comment.parent_id,
+                                                   comment.name,
+                                                   comment.body,
+                                                   comment.ups,
+                                                   comment.downs,
+                                                   comment.controversiality,
+                                                   comment.total_awards_received,
+                                                   comment.score,
+                                                   comment.locked,
+                                                   comment.collapsed,
+                                                   comment.is_submitter,
+                                                   comment.created_utc])
         posts_df = pd.DataFrame(posts_retrieved,
-                            columns=['post_id','title', 'score','subreddit','url',
-                                        'num_comments', 'body', 'created'])
+                                columns=['post_id', 'title', 'score', 'upvote_ratio', 'subreddit',' url',
+                                         'num_comments', 'body', 'created'])
         comments_df = pd.DataFrame(comments_retrieved,
-                                columns=['post_id','comment_id','comment','up_vote_count',
-                                        'down_vote_count','controversiality', 'total_awards_received',
-                                        'score','is_locked','is_collapsed'
-                                        ])
+                                   columns=['post_id', 'comment_id', 'parent_id', 'name', 'comment',
+                                            'up_vote_count', 'down_vote_count', 'controversiality',
+                                            'total_awards_received', 'score', 'is_locked', 'is_collapsed',
+                                            'is_submitter', 'created_utc'])
         return SubRedditData(subreddit_name, posts_df, comments_df)
 
     @async_retry(times=3, delay=10)
     async def scrape(self):
-        logger.info(f'start scraping top {self.config.max_post_count} hotest posts with '
-                   f'{self.config.max_comment_count} comments per post from the following subreddits: '
+
+        scrape_order_str = {
+            'hot': 'hottest',
+            'new': 'newest',
+            'top': 'top',
+            'rising': 'rising'
+        }
+
+        logger.info(f'start scraping top {self.config.max_post_count} {scrape_order_str[self.config.scrape_order]} posts with '
+                    f'{self.config.max_comment_count} comments per post from the following subreddits: '
                     f'{", ".join(self.config.subreddit_list)}')
         try:
             self._data = await asyncio.gather(*(self._fetch_from_single_subreddit(subreddit_name) for
-                                            subreddit_name in self.config.subreddit_list))
+                                              subreddit_name in self.config.subreddit_list))
         except Exception:
             logger.error("failed to scrape, retrying...")
             raise
@@ -130,12 +182,13 @@ class AsyncRedditScraper:
     def save_to_file(self):
         if not self._data:
             pass
-        p = Path('.').joinpath('data')
-        p.mkdir(exist_ok=True)
+        p = get_project_root().joinpath('data/raw')
+        p.mkdir(parents=True, exist_ok=True)
         for data_obj in self._data:
-            data_obj.posts_df.to_csv(p.joinpath(f'{data_obj.name}_posts.csv'), index=False)
-            data_obj.comments_df.to_csv(p.joinpath(f'{data_obj.name}_comments.csv'), index = False)
+            data_obj.posts_df.to_csv(p.joinpath(f'{data_obj.name}_{self.config.scrape_order}_posts.csv'), index=False)
+            data_obj.comments_df.to_csv(p.joinpath(f'{data_obj.name}_{self.config.scrape_order}_comments.csv'), index=False)
         return self
+
 
 async def main():
     scraper = AsyncRedditScraper(default_config)
