64aeeabd92074cffa0fa9970b939ae70fce6496c
azhou5211 <azhou5211@gmail.com>
2022-02-11 20:33:05 +0000

    pushing finalized relevance module

--- src/models/relevance.py
+++ src/models/relevance.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from src.utils import get_project_root
+from src.features.preprocess import PreProcess
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -20,12 +21,20 @@ class Relevance:
     def generate_relevance(self, df):
         """
 
-        :param df:
-        :return:
+        :param df: The dataframe to run relevance on. Make sure the dataframe has posts and comments merged
+        :return: The dataframe with relevance score column
         """
+        
+        # First fill na for empty titles, bodies, and comments
+        title_body_comment = pd.DataFrame(df[['post_id', 'title', 'body', 'comment']])
+        pp = PreProcess()
+        pp.fill_na(title_body_comment, 'title')
+        pp.fill_na(title_body_comment, 'body')
+        # pp.fill_na(title_body_comment, 'comment')
+        
         similarities = np.array([])
-        for post_id in df['post_id'].unique():
-            temp = df[df['post_id'] == post_id]
+        for post_id in title_body_comment['post_id'].unique():
+            temp = title_body_comment[title_body_comment['post_id'] == post_id]
             if pd.isnull(temp.iloc[0]['comment']):
                 similarities = np.append(similarities, 0)
                 continue
@@ -43,16 +52,17 @@ class Relevance:
             comments = temp['comment'].to_list()
             comments.append(topic)
 
-            sentence_embeddings = model.encode(comments)
+            sentence_embeddings = self.model.encode(comments)
             similarity = cosine_similarity([sentence_embeddings[-1]], sentence_embeddings[:-1])
             similarities = np.append(similarities, similarity.flatten())
-            df['relevance'] = similarities
-            self.df = df
-            return df
+        df['relevance'] = similarities
+        self.df = df
+        return df
 
     def save_to_file(self, path: str = 'data/results', file_name: str = 'relevance.csv'):
-        if not self.df:
-            raise Exception("Call generate_relevance first before save_to_file")
-        p = get_project_root().joinpath(path)
-        p.mkdir(parents=True, exist_ok=True)
-        self.df.to_csv(p.joinpath(file_name), index=False)
+        if self.df is not None:
+            p = get_project_root().joinpath(path)
+            p.mkdir(parents=True, exist_ok=True)
+            self.df.to_csv(p.joinpath(file_name), index=False)
+        else:
+            raise Exception("Missing relevance. Call generate_relevance first before save_to_file")
