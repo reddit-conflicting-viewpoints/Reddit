74f227b367713e38a9f2a20c25b0a7e75d4fba39
azhou5211 <azhou5211@gmail.com>
2022-03-08 04:21:05 +0000

    added a few more plots

--- pages/sentimentanalysis_page.py
+++ pages/sentimentanalysis_page.py
@@ -7,14 +7,20 @@ layout =html.Div([
             html.H1('Sentiment Analysis - Can we identify conflict with Sentiment?',style={'textAlign':'center'}),
             dcc.Loading(children=[
                 html.H3(id='sentimentsubredditprinter',style={'textAlign':'center'}),
+                dcc.Graph(id='sentiment1'),
+                dcc.Graph(id='sentiment2'),
+                dcc.Graph(id='sentiment3'),
             ], fullscreen=True),
         ])
 
 @callback(
+    Output('sentiment1', 'figure'),
+    Output('sentiment2', 'figure'),
+    Output('sentiment3', 'figure'),
     Output('sentimentsubredditprinter', 'children'),
     Input('session', 'data')
 )
 def update_graph(data):
     df = pd.DataFrame(data)
     subreddit = df.at[0, 'subreddit']
-    return f'Subreddit: {subreddit}'
+    return px.box(df, x="comment_sentiment", y='comment_relevance'), px.histogram(df, x="comment_sentiment"), px.box(df, x="post_id", y='comment_relevance'), f'Subreddit: {subreddit}'
