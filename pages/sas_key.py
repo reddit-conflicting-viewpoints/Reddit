df945bf155792865cafc6fddda6566e46f52d7c3
azhou5211 <azhou5211@gmail.com>
2022-03-08 20:40:25 +0000

    added more content to home page

--- pages/sas_key.py
+++ pages/sas_key.py
@@ -5,8 +5,11 @@ from pandas.errors import ParserError
 AZURE_URL = "https://redditconflict.blob.core.windows.net/redditconflict/results/"
 SAS_KEY = "?sp=r&st=2022-03-06T23:29:32Z&se=2023-01-01T07:29:32Z&sv=2020-08-04&sr=c&sig=%2FGHaPmXaEBpc36JBlYfMJLPE8dYr5cFcAsbPpjV16NA%3D"
 
-def get_csv_url(subreddit, sort_order='hot'):
-    return AZURE_URL + subreddit + "_" + sort_order + "_results.csv" + SAS_KEY
+def get_csv_url(subreddit, sort_order='hot', results=True):
+    if results:
+        return AZURE_URL + subreddit + "_" + sort_order + "_results.csv" + SAS_KEY
+    else:
+        return AZURE_URL + subreddit + "_" + sort_order + "_subreddit.csv" + SAS_KEY
 
 def get_df(subreddit, sort_order='hot'):
     dtypess ={
@@ -50,4 +53,11 @@ def get_df(subreddit, sort_order='hot'):
     except ParserError:
         df = pd.read_csv(get_csv_url(subreddit, sort_order), lineterminator='\n')
         # df = pd.read_csv(get_csv_url(subreddit, sort_order), lineterminator='\n', dtype=dtypess)
+    return df
+
+def get_df_description(subreddit, sort_order='hot'):
+    try:
+        df = pd.read_csv(get_csv_url(subreddit, sort_order, False))
+    except ParserError:
+        df = pd.read_csv(get_csv_url(subreddit, sort_order, False), lineterminator='\n')
     return df
