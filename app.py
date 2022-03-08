6640ba45ce857dbf75a760208112306e7f9d57c7
azhou5211 <azhou5211@gmail.com>
2022-03-08 00:57:12 +0000

    modified app to run in a single file. importing files is a lot quicker now

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 # Code source: https://dash-bootstrap-components.opensource.faculty.ai/examples/simple-sidebar/
 import dash
-from dash import html, dcc
+from dash import html, dcc, callback
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import plotly.express as px
-from pages import relevance_page, sentimentanalysis_page, topicmodeling_page
+from pages.sas_key import get_df
+from pages.visualize import *
+#from pages import relevance_page, sentimentanalysis_page, topicmodeling_page
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 app.title = "BEReddiT"
 
 server = app.server
 
+global df
+global subreddit
+
 # Read the list of available subreddits
 file1 = open("pages/subreddit_list.txt", "r")
 subreddits = file1.readlines()
-subreddits = list(map(lambda item: item.replace('\n', ''), subreddits))
+subreddits = list(set(map(lambda item: item.replace('\n', ''), subreddits)))
+subreddits.sort()
 file1.close()
 
 # styling the sidebar
@@ -66,7 +72,7 @@ content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
 
 app.layout = html.Div([
     dcc.Location(id="url"),
-    dcc.Store(id='session', storage_type='session', data='computerscience'),
+    dcc.Loading(children=[dcc.Store(id='session', storage_type='local')], fullscreen=True),
     sidebar,
     content
 ])
@@ -77,34 +83,90 @@ app.layout = html.Div([
     [Input("url", "pathname")]
 )
 def render_page_content(pathname):
-    if pathname == "/":
-        # Can add more items to the list, for more content in homepage.
-        return html.Div([
+    try:
+        ### Home Page
+        if pathname == "/":
+            # Can add more items to the list, for more content in homepage.
+            return  html.Div([
 
-                    # Welcome Header
-                    html.H1('Welcome to BEReddiT', style={'textAlign':'center'}),
+                        # Welcome Header
+                        html.H1('Welcome to BEReddiT', style={'textAlign':'center'}),
 
-                    # Introduction Paragraph
-                    html.Div([
-                        html.P('Add an Introduction here!', style={'textAlign':'center'})
-                    ], style=CENTER_STYLE),
+                        # Introduction Paragraph
+                        html.Div([
+                            html.P('Add an Introduction here!', style={'textAlign':'center'})
+                        ], style=CENTER_STYLE),
 
-                    # Drop down menu for selecting SubReddit
-                    html.Div([
-                        html.P('Pick a SubReddit to Analyze', style={'textAlign':'center'}),
-                        dcc.Dropdown(
-                            subreddits,
-                            "computerscience",
-                            id='data'
-                        )
-                    ], style=CENTER_STYLE)
-                ])
-    elif pathname == "/relevance":
-        return relevance_page.layout
-    elif pathname == "/topicmodeling":
-        return topicmodeling_page.layout
-    elif pathname == "/sentimentanalysis":
-        return sentimentanalysis_page.layout
+                        # Drop down menu for selecting SubReddit
+                        html.Div([
+                            html.P('Pick a SubReddit to Analyze', style={'textAlign':'center'}),
+                            dcc.Dropdown(
+                                subreddits,
+                                "computerscience",
+                                id='data'
+                            )
+                        ], style=CENTER_STYLE)
+                    ])
+
+        ### Relevance Page
+        elif pathname == "/relevance":
+            return  html.Div([
+                        html.H1('Relevance - Are the comments in discussions relevant to the submission?',style={'textAlign':'center'}),
+                        html.H3(f'Subreddit: {subreddit}',style={'textAlign':'center'}),
+                        dcc.Loading(children=[dcc.Graph(id='first')], fullscreen=True),
+                    ])
+
+        ### Topic Modeling Page
+        elif pathname == "/topicmodeling":
+            return  html.Div([
+                        html.H1('Topic Modeling - What are posts talking about?',style={'textAlign':'center'}),
+                        html.H3(f'Subreddit: {subreddit}',style={'textAlign':'center'}),
+                    ])
+
+        ### Sentiment Page
+        elif pathname == "/sentimentanalysis":
+            return  html.Div([
+                        html.H1('Sentiment Analysis - Can we identify conflict with Sentiment?',style={'textAlign':'center'}),
+                        html.H3(f'Subreddit: {subreddit}',style={'textAlign':'center'}),
+                        html.Div([
+                            html.Div([
+                                html.P("X-Axis", style={'textAlign':'center'}),
+                                dcc.Dropdown(id='xaxis-column', options=df.columns, value='post_score'),
+                                dcc.RadioItems(
+                                    ['Linear', 'Log'],
+                                    'Linear',
+                                    id='xaxis-type',
+                                    inline=True
+                                )
+                            ], style={'width': '48%', 'display': 'inline-block'}),
+
+                            html.Div([
+                                html.P("Y-Axis", style={'textAlign':'center'}),
+                                dcc.Dropdown(id='yaxis-column', options=df.columns, value='post_upvote_ratio'),
+                                dcc.RadioItems(
+                                    ['Linear', 'Log'],
+                                    'Linear',
+                                    id='yaxis-type',
+                                    inline=True
+                                )
+                            ], style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
+                        ]),
+
+                        dcc.Loading(children=[dcc.Graph(id='indicator-graphic')], fullscreen=True),
+
+                        # dcc.Slider(
+                        #     df['Year'].min(),
+                        #     df['Year'].max(),
+                        #     step=None,
+                        #     id='year--slider',
+                        #     value=df['Year'].max(),
+                        #     marks={str(year): str(year) for year in df['Year'].unique()},
+                        # )
+                    ])
+    except NameError:
+        return html.H1('No data loaded. Head to Home Page First!', style={'textAlign':'center'})
+
+    ### 404 Page
     # If the user tries to reach a different page, return a 404 message
     return html.H1('404: Page Not Found', style={'textAlign':'center'})
 
@@ -113,7 +175,47 @@ def render_page_content(pathname):
     Input('data', 'value')
 )
 def update_df(value):
-    return value
+    global df
+    df = get_df(value)
+    global subreddit
+    subreddit = value
+    return ""
+
+@callback(
+    Output('first', 'figure'),
+    Input('session', 'data')
+)
+def update_graph(data):
+    global df
+    global subreddit
+    return px.histogram(df, x="comment_relevance", title=f'Comment Relevance Histogram (Subreddit: {subreddit})')
+    # return plot('scatter', df, x_col='comment_relevance', y_col='comment_score', title=subreddit, threshold_col='comment_relevance', threshold_min=0.5, threshold_max=1)
+
+@callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis-column', 'value'),
+    Input('yaxis-column', 'value'),
+    Input('xaxis-type', 'value'),
+    Input('yaxis-type', 'value'),
+    Input('session', 'data')
+)
+def update_graph2(xaxis_column_name, yaxis_column_name,
+                 xaxis_type, yaxis_type, data):
+    global subreddit
+    global df
+
+    fig = px.scatter(x=df[xaxis_column_name],
+                     y=df[yaxis_column_name],
+                     hover_name=df['post_id'].apply(lambda s: "Post ID: " + s))
+
+    # fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    fig.update_xaxes(title=xaxis_column_name,
+                     type='linear' if xaxis_type == 'Linear' else 'log')
+
+    fig.update_yaxes(title=yaxis_column_name,
+                     type='linear' if yaxis_type == 'Linear' else 'log')
+    return fig
 
 
 if __name__=='__main__':
--- pages/sas_key.py
+++ pages/sas_key.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from pandas.errors import ParserError
 
 AZURE_URL = "https://redditconflict.blob.core.windows.net/redditconflict/results/"
@@ -8,8 +9,45 @@ def get_csv_url(subreddit, sort_order='hot'):
     return AZURE_URL + subreddit + "_" + sort_order + "_results.csv" + SAS_KEY
 
 def get_df(subreddit, sort_order='hot'):
+    dtypess ={
+        'post_index': int,
+        'post_id': str,
+        'post_title': str,
+        'flair': str,
+        'post_score': int,
+        'post_upvote_ratio': np.float64,
+        'subreddit': str,
+        'post_url': str,
+        'num_comments': int,
+        'post_body': str,
+        'post_created': int,
+        'post_body_word_token': str,
+        'post_body_tag': str,
+        'post_body_string': str,
+        'post_topics': str,
+        'post_sentiment': int,
+        'comment_index': pd.Int64Dtype(),
+        'comment_id': str,
+        'parent_id': str,
+        'comment': str,
+        'comment_score': pd.Int64Dtype(),
+        'comment_controversiality': pd.Int64Dtype(),
+        'comment_total_awards_received': pd.Int64Dtype(),
+        'comment_is_locked': pd.BooleanDtype(),
+        'comment_is_collapsed': pd.BooleanDtype(),
+        'comment_is_submitter': pd.BooleanDtype(),
+        'comment_created': pd.Int64Dtype(),
+        'comment_word_token': str,
+        'comment_tag': str,
+        'comment_body_string': str,
+        'comment_topics': str,
+        'comment_sentiment': pd.Int64Dtype(),
+        'comment_relevance': np.float64
+    }
     try:
         df = pd.read_csv(get_csv_url(subreddit, sort_order))
+        # df = pd.read_csv(get_csv_url(subreddit, sort_order), dtype=dtypess)
     except ParserError:
         df = pd.read_csv(get_csv_url(subreddit, sort_order), lineterminator='\n')
+        # df = pd.read_csv(get_csv_url(subreddit, sort_order), lineterminator='\n', dtype=dtypess)
     return df
