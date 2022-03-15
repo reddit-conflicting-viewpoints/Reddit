3fea0bb6c9a1fbf9322929ead6a94518068d6b92
smuktevi <vmuktev1@uw.edu>
2022-03-15 16:31:35 -0700

    minor ui changes to conflicting viewpoints page

--- pages/conflictingviewpoints_page.py
+++ pages/conflictingviewpoints_page.py
@@ -70,7 +70,7 @@ def update_graph(data):
         fig1_df = fig1_df.droplevel(1, axis=1)
         cols = ['comment_topics', 'comment_relevance_mean', 'comment_relevance_size', 'sentiment_diff_mean', 'sentiment_diff_size']
         fig1_df.columns = cols
-        controversy_relevance_plot = cv_plot('scatter', fig1_df[1:], x_col='comment_relevance_mean', y_col='sentiment_diff_mean', size='sentiment_diff_size', title='r/'+subreddit, hover_name='comment_topics', labels={
+        controversy_relevance_plot = cv_plot('scatter', fig1_df[1:], x_col='comment_relevance_mean', y_col='sentiment_diff_mean', size='sentiment_diff_size', title='Conflicting Viewpoints in r/'+subreddit, hover_name='comment_topics', labels={
                      "sentiment_diff_mean": "Average Controversy",
                      "comment_relevance_mean": "Average Comment Relevance"
                  })
@@ -115,7 +115,7 @@ def update_graph_2(data, post_id):
                      "index": "Number of Comments",
                      "value": "Rolling Average Comment Sentiment (5 Comments)",
                      "variable": "Rolling Average Comment Sentiment (5 Comments)"
-                 }, title=post_title + ' - ' + subreddit)
+                 }, title='Sentiment in Post:"'+post_title + '" - r/' + subreddit)
         comment_series_1_plot.update_layout(showlegend=False)
 
         ### Relevance Plot
@@ -126,7 +126,7 @@ def update_graph_2(data, post_id):
                      "index": "Number of Comments",
                      "value": "Rolling Average Comment Relevance (5 Comments)",
                      "variable": "Rolling Average Comment Relevance (5 Comments)"
-                 }, title=post_title + ' - ' + subreddit)
+                 }, title='Relevance in Post:"'+post_title + '" - r/' + subreddit)
         comment_series_2_plot.update_layout(showlegend=False)
 
         ### Controversy Plot
